999a9584558436eb2422b2e0d48eaf793a1e419a
Linda <linda.latreider@web.de>
2015-02-04 23:02:09 +0100

    Umstrukturierung Listenausgabe/-bearbeitung
    - Liste ausgeben/bearbeiten jederzeit aufrufbar
    - default ausgegliedert um Schleifen-Fehler zu finden

--- main.c
+++ main.c
@@ -22,17 +22,17 @@ int main()
         {
         case 'R': case 'r': printf("Tempor%cre Wartungsarbeiten. Wir bitten um Geduld.\n", ae); break; //Funktionsaufruf: Listenbearbeitung
         case 'A': case 'a':
-            printf("\nL = Liste ausgeben\nB = Liste bearbeiten\n");
-            scanf("%c",&userentry);
-                if ((userentry=='L')||(userentry=='l'))
-                    {printf("ist in Arbeit"); break;} //TODO
-                else if ((userentry =='B')||(userentry=='b'))
-                    {func_add(); break;}   //Testen
+            printf("\nL = Liste ausgeben\nB = Liste bearbeiten\n"); break;
+            case 'L': case 'l': printf("ist in Arbeit"); break; //TODO
+            case 'B': case 'b': func_add(); break;   //Testen
         case 'Q': case 'q': runloop=0;
         break;
-        default:
-        printf("Unbekannte Eingabe.\nFolgende Befehle sind zul%cssig:\n", ae);
-        printf("R = Routenberechnung\nA = Anzeige und Bearbeitung der Ausfahrten und Autobahnkreuze\nQ = Beenden\n\n");
+        }
+        if (userentry!='R'&&userentry!='r'&&userentry!='A'&&userentry!='a'&&userentry!='L'&&userentry!='l'
+            &&userentry!='B'&&userentry!='b'&&userentry!='Q'&&userentry!='q')
+        {
+            printf("Unbekannte Eingabe.\nFolgende Befehle sind zul%cssig:\n", ae);
+            printf("R = Routenberechnung\nA = Anzeige und Bearbeitung der Ausfahrten und Autobahnkreuze\nQ = Beenden\n\n");
         }
     } while (runloop);
 
