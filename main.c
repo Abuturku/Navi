9bdcb0c2bf4d97299c62bf7c442f79c6517cc01a
Linda <linda.latreider@web.de>
2015-02-04 23:23:24 +0100

    Funktionierende Main-Funktion
    - Bugs in Do-While-Schleife gefixed.
    - Alle TODOs in Main gekennzeichnet
    - Platzhalter für kommende Funktionen platziert

--- main.c
+++ main.c
@@ -11,27 +11,29 @@ int main()
 
     //Begrüßungstext und Erläuterung der Optionen
     printf("Willkommen bei Ihrem Autobahnnavigator!\nDies sind Ihre Eingabeoptionen:\n\n");
-    printf("R = Routenberechnung\nA = Anzeige und Bearbeitung der Ausfahrten und Autobahnkreuze\nQ = Beenden\n\n");
+    printf("R = Routenberechnung\nA = Anzeige und Bearbeitung der Ausfahrten und Autobahnkreuze\nX = Beenden\n\n");
 
     //Menü-Schleife
     do
     {
-        scanf("%c", &userentry);
-        switch(userentry)
+        fflush(stdin);              //Löschen des Buffers, damit Scanf bei jedem Schleifendurchlauf ausgeführt wird
+        scanf("%c", &userentry);    //Einlesen der Benutzereingabe
+        switch(userentry)           //Überprüfen der Benutzereingabe
         {
-        case 'R': case 'r': printf("Tempor%cre Wartungsarbeiten. Wir bitten um Geduld.\n", ae); break; //Funktionsaufruf: Listenbearbeitung
+        //Routenberechnung
+        case 'R': case 'r': printf("Tempor%cre Wartungsarbeiten. Wir bitten um Geduld.\n", ae); break; //TODO: Funktionsaufruf: Listenbearbeitung
+        //Listenansicht
         case 'A': case 'a':
             printf("\nL = Liste ausgeben\nB = Liste bearbeiten\n"); break;
-            case 'L': case 'l': printf("ist in Arbeit"); break; //TODO
+            case 'L': case 'l': printf("ist in Arbeit\n"); break; //TODO -> Liste von Ausfahrten oder Kreuzen?; -> Autobahnnummer? (Bei 0 alle ausgeben).....
             case 'B': case 'b': func_add(); break;   //Testen
-        case 'Q': case 'q': runloop=0;
+        //Exit
+        case 'X': case 'x': runloop=0;
         break;
-        }
-        if (userentry!='R'&&userentry!='r'&&userentry!='A'&&userentry!='a'&&userentry!='L'&&userentry!='l'
-            &&userentry!='B'&&userentry!='b'&&userentry!='Q'&&userentry!='q')
-        {
-            printf("Unbekannte Eingabe.\nFolgende Befehle sind zul%cssig:\n", ae);
-            printf("R = Routenberechnung\nA = Anzeige und Bearbeitung der Ausfahrten und Autobahnkreuze\nQ = Beenden\n\n");
+        //Abfangen Fehleingabe
+        default:
+            printf("\nUnbekannte Eingabe.\nFolgende Befehle sind zul%cssig:\n", ae);
+            printf("R = Routenberechnung\nA = Anzeige und Bearbeitung der Ausfahrten und Autobahnkreuze\nX = Beenden\n\n");
         }
     } while (runloop);
 
