0c7f4b87afb66aa17b69962f10693f7492158f9c
Andy <andreasschick95@gmail.com>
2015-02-04 21:54:23 +0100

    main.c
    main.c

--- main.c
+++ main.c
@@ -1,10 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <stdbool.h>
 #include "list_functions.c"
+#include "list_sort.c"
 
 int main()
 {
+    bool runloop = 1; //Variable zum Beenden der Menü-Schleife
+    char userentry; //Variable zur Benutzereingabe
+
+    //Begrüßungstext und Erläuterung der Optionen
+    printf("Willkommen bei Ihrem Autobahnnavigator!\nDies sind Ihre Eingabeoptionen:\n\n");
+    printf("R = Routenberechnung\nA = Anzeige und Bearbeitung der Ausfahrten und Autobahnkreuze\nB = Beenden\n\n");
+
+    //Menü-Schleife
+    do
+    {
+        scanf("%c", &userentry);
+        switch(userentry)
+        {
+        case 'R': case 'r': printf("Die Eingabe war R\n"); break;   //vorläufig
+        case 'A': case 'a': printf("Die Eingabe war A\n"); break;
+        case 'B': case 'b': runloop=0;
+        break;
+        default:
+        printf("Unbekannte Eingabe.\nFolgende Befehle sind zulaessig:\n");
+        printf("R = Routenberechnung\nA = Anzeige und Bearbeitung der Ausfahrten und Autobahnkreuze\nB = Beenden\n\n");
+        }
+    } while (runloop);
+
+
+/*
 
     // Hier mögt ihr eure Kekse
 
@@ -23,6 +49,6 @@ int main()
     if(testvar == 2)
     {
         return 0;
-    }
+    }*/
     return 0;
 }
