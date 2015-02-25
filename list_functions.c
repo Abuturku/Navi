3b7e5ee26145f2997433b933ef151b69b20f9da1
Andy <andreasschick95@gmail.com>
2015-02-25 10:54:33 +0100

    Revert "Revert "Irgendwas hats zerschossen""
    This reverts commit 9dc11e875355f6762ba3c522589b30515bfb9669.

--- list_functions.c
+++ list_functions.c
@@ -1,7 +1,7 @@
 /* -------------------------------------------------------------------------------------------------
 Elsemann, Andreas       and.elsemann.14@dhbw-mosbach.de         Kurs INF14B Wintersemester 2014/2015
 Latreider, Linda        lin.latreider.14@dhbw-mosbach.de        Kurs INF14B Wintersemester 2014/2015
-Schick, Andreas         and.schick@dhbw-mosbach.de              Kurs INF14B Wintersemester 2014/2015
+Schick, Andreas         and.schick.14@dhbw-mosbach.de              Kurs INF14B Wintersemester 2014/2015
 Scholz, Oliver          oli.scholz.14@dhbw-mosbach.de           Kurs INF14B Wintersemester 2014/2015
 
 DHBW Mosbach
